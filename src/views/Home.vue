8331fdebea58a1489148e97d901b2908ee5c2f8f
Korolkovart <56297922+Korolkovart@users.noreply.github.com>
2020-05-20 16:24:28 +0300

    Todo application

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quaerat quis delectus ipsa, neque eveniet iusto facilis tenetur soluta ipsam ut explicabo eum vel nobis et fugit distinctio, unde aliquam!</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
